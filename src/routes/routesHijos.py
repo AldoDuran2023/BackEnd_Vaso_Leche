5edf1178e00ebdf193320c3756813eeb20356c43
Vitaliano <aldoduranrojas440@gmail.com>
2025-06-26 17:08:45 -0500

    api hijos

--- src/routes/routesHijos.py
+++ src/routes/routesHijos.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from src.services.servicesHijos import insert_hijo
+from src.services.servicesHijos import insert_hijo, lista_de_hijos
 
 hijos = Blueprint('hijos', __name__)
 
@@ -7,3 +7,8 @@ hijos = Blueprint('hijos', __name__)
 @hijos.route('/nuevo', methods=['POST'])
 def registrar_hijo():
     return insert_hijo()
+
+# ruta para obtener todos los hijos de una beneficiaria
+@hijos.route('/<int:id_beneficiaria>', methods=['GET'])
+def obtener_hijos(id_beneficiaria):
+    return lista_de_hijos(id_beneficiaria)
--- src/services/servicesHijos.py
+++ src/services/servicesHijos.py
@@ -22,7 +22,6 @@ def insert_hijo():
             direccion, fecha_nacimiento, partida, dni_madre
         ))
 
-        # 🚨 Muy importante para evitar el error 2014
         while cursor.nextset():
             pass
 
@@ -41,3 +40,32 @@ def insert_hijo():
 
     finally:
         cursor.close()
+        
+# funcion para devolver a los hijos de una beneficiaria
+def lista_de_hijos (id):
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM vista_hijos where fk_beneficiaria = %s',(id,))
+        result = cursor.fetchall()
+        cursor.close()
+        hijos_list = []
+        for row in result:
+            hijos_list.append({
+                'id_hijo': row[2],
+                'DNI': row[3],
+                'nombres': row[4],
+                'edad': row[5],
+                'partida': row[6],
+                'direccion': row[7],
+                'estado': row[8]
+            })
+        return jsonify({
+            'success': True,
+            'data': hijos_list,
+            'message': 'Hijos cargados correctamente'
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'message': str(e)
+        })
